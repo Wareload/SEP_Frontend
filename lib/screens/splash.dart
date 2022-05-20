6f00a7a2df99e39f2df337ea50908d1ea9257bdb
Wareload <wareload.developer@gmail.com>
2022-05-20 16:53:31 +0200

    remake splash screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -11,31 +11,45 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  List<Color> colorList = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow
+  ];
+  List<Alignment> alignmentList = [
+    Alignment.bottomLeft,
+    Alignment.bottomRight,
+    Alignment.topRight,
+    Alignment.topLeft,
+  ];
+  int index = 0;
+  Color bottomColor = Colors.red;
+  Color topColor = Colors.yellow;
+  Alignment begin = Alignment.bottomLeft;
+  Alignment end = Alignment.topRight;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(child: LayoutBuilder(builder: (builder, constraints) {
-      return Container(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 0.5 * constraints.maxHeight),
-          decoration: BoxDecoration(
-              gradient: Gradient.lerp(
-                  const LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.blue,
-                        Colors.purple
-                      ]),
-                  const LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      colors: [
-                        Colors.green,
-                        Colors.redAccent
-                      ]),
-                 0.3)),
-          alignment: Alignment.center,
-          child: Widgets.getTextFieldH1("Moody", constraints));
+      return AnimatedContainer(
+        width: constraints.maxWidth,
+        height: constraints.maxHeight,
+        duration: const Duration(milliseconds: 800),
+        child: Container(alignment: Alignment.center, padding: EdgeInsets.fromLTRB(0, 0, 0, constraints.maxHeight*0.4), child: Widgets.getTextFieldH1("Moody", constraints)),
+        onEnd: () {
+          setState(() {
+            index = index + 1;
+            // animate the color
+            bottomColor = colorList[index % colorList.length];
+            topColor = colorList[(index + 1) % colorList.length];
+          });
+        },
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: begin, end: end, colors: [bottomColor, topColor])),
+      );
     })));
   }
 
@@ -43,6 +57,16 @@ class _SplashState extends State<Splash> {
   void initState() {
     super.initState();
     init();
+    _startAnimation();
+  }
+
+  Widget _startAnimation() {
+    Future.delayed(const Duration(milliseconds: 10), () {
+      setState(() {
+        bottomColor = Colors.blue;
+      });
+    });
+    return Container();
   }
 
   void init() {
@@ -50,9 +74,9 @@ class _SplashState extends State<Splash> {
       await Settings.setApi();
       //await Settings.api.logout(); //use to force clear flutter secure storage at start
       if (await Settings.api.isLoggedIn()) {
-        Navigator.pushReplacementNamed(context, RouteGenerator.teamOverview);
+           Navigator.pushReplacementNamed(context, RouteGenerator.teamOverview);
       } else {
-        Navigator.pushReplacementNamed(context, RouteGenerator.login);
+           Navigator.pushReplacementNamed(context, RouteGenerator.login);
       }
     });
   }
