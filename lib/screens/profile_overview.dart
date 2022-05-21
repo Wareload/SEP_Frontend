0aa5fb700a223e1d89323e9ca5690db0d8bf1ded
Wareload <wareload.developer@gmail.com>
2022-05-21 05:32:42 +0200

    remake profile page

--- lib/screens/profile_overview.dart
+++ lib/screens/profile_overview.dart
@@ -13,27 +13,94 @@ class _ProfileOverviewState extends State<ProfileOverview> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(child: LayoutBuilder(builder: (builder, constraints) {
-          return Column(mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: constraints.maxWidth*0.05,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Widgets.getTextButtonStyle1("Back", _back, constraints),
-                  Widgets.getTextFieldH3C("Dein Profil", constraints),
-                  Widgets.getTextButtonStyle1("Einstellungen", () => {}, constraints),
-                ],
-              ),
-
+              IconButton(
+                  onPressed: _back,
+                  icon: const Icon(Icons.chevron_left,
+                      color: Colors.blue, size: 48.0)),
+              Widgets.getTextFieldH3C("Dein Profil", constraints),
+              IconButton(
+                padding: EdgeInsets.only(right: 0.08*constraints.maxWidth),
+                  onPressed: () => {},
+                  icon: const Icon(Icons.settings,
+                      color: Colors.blue, size: 48.0)),
             ],
-          );
+          ),
+          Widgets.getProfileImage(
+              "https://bugsbunnies.de/images/logo.png", constraints),
+          Widgets.getTextFieldH2("David Neus", constraints),
+          Widgets.getTextFieldH3("david.neus@gmx.de", constraints),
+          _getTags(constraints),
+          Container(
+              margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+              child: Align(
+                  child: Widgets.getTextFieldH3("Deine Teams:", constraints),
+                  alignment: Alignment.centerLeft)),
+          Expanded(
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Widgets.getProfileTeam(
+                    "Team1", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team2", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team3", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team4", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team4", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team4", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team6", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team7", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team8", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team9", () => {}, () => {}, constraints),
+                Widgets.getProfileTeam(
+                    "Team10", () => {}, () => {}, constraints),
+              ],
+            ),
+          ))
+        ],
+      );
     })));
   }
 
-  void _back(){
+  Widget _getTags(BoxConstraints constraints) {
+    return Container(
+        margin: const EdgeInsets.all(10),
+        height: 40,
+        child: Center(
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            children: [
+              Widgets.getContainerTag("Only", constraints),
+              Widgets.getContainerTag("Fans", constraints),
+              Widgets.getContainerTag("Jokes", constraints),
+              Widgets.getContainerTag("Javascript", constraints),
+              Widgets.getContainerTag("LGBTQ+HDTV_QHD", constraints),
+              Widgets.getContainerTag("LGBTQ+HDTV_QHD", constraints),
+              Widgets.getContainerTag("LGBTQ+HDTV_QHD", constraints),
+            ],
+          ),
+        ));
+  }
+
+  void _back() {
     Navigator.pop(context);
   }
 
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -122,4 +122,96 @@ class Widgets {
           ),
         ));
   }
+
+  static Widget getProfileImage(String link, BoxConstraints constraints) {
+    return Container(
+      margin: EdgeInsets.only(top: constraints.maxWidth * 0.05),
+      width: constraints.maxWidth * 0.5,
+      height: constraints.maxWidth * 0.5,
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          shape: BoxShape.circle,
+          image: DecorationImage(
+              fit: BoxFit.fitHeight, image: NetworkImage(link))),
+      // child: Image.network(link),
+    );
+  }
+
+  static Widget getContainerTag(String text, BoxConstraints constraints) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(constraints.maxWidth * 0.03, 0, 0, 0),
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      //color: Colors.amber,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+        alignment: Alignment.center,
+        child: Text(
+          text,
+          style: const TextStyle(
+              fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      decoration: BoxDecoration(
+          color: Colors.orange,
+          border: Border.all(
+            color: Colors.deepOrangeAccent,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(20))),
+    );
+  }
+
+  static Widget getProfileTeam(display, VoidCallback click, VoidCallback leave,
+      BoxConstraints constraints) {
+    return Container(
+        margin: EdgeInsets.only(bottom: constraints.maxWidth * 0.02),
+        child: Row(
+          children: [
+            Container(
+              width: constraints.maxWidth * 0.75,
+              margin: EdgeInsets.only(left: constraints.maxWidth * 0.05),
+              child: Material(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(50),
+                child: InkWell(
+                  onTap: click,
+                  borderRadius: BorderRadius.circular(50),
+                  child: Container(
+                    height: constraints.maxWidth * 0.15,
+                    alignment: Alignment.center,
+                    child: Text(
+                      display,
+                      style: const TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: constraints.maxWidth * 0.02),
+                child: Center(
+                  child: Material(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(50),
+                    child: InkWell(
+                      onTap: leave,
+                      borderRadius: BorderRadius.circular(50),
+                      child: Container(
+                        width: constraints.maxWidth * 0.14,
+                        height: constraints.maxWidth * 0.14,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "-",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 40, color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                  ),
+                ))
+          ],
+        ));
+  }
 }
