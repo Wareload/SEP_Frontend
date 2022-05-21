536405786eb67793aba38e04316e0eddcdc2a14c
Wareload <wareload.developer@gmail.com>
2022-05-21 07:43:19 +0200

    disable landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'router/route_generator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
