85f6b40e0bad05f0f6fd7db454bf3abd466192b1
Chri$ <ujail@protonmail.com>
2022-06-27 16:42:11 +0200

    created notification method

---
+++ lib/screens/notification_select.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:moody/api/api.dart';
+import 'package:moody/route/route_generator.dart';
+import 'package:moody/widgets/settings.dart';
+import 'package:moody/widgets/widgets.dart';
+
+import '../structs/team.dart';
+
+class NotificationSelect extends StatefulWidget {
+  const NotificationSelect({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _NotificationSelectState();
+}
+
+class _NotificationSelectState extends State<NotificationSelect> {
+  TextEditingController noteController = TextEditingController();
+
+  TimeOfDay? time = const TimeOfDay(hour: 12, minute: 12);
+
+  @override
+  Widget build(BuildContext context) {
+    var args = (ModalRoute.of(context)?.settings.arguments ??
+        <String, dynamic>{}) as Map;
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(child: LayoutBuilder(builder: (builder, constraints) {
+          return CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Center(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Widgets.getNavBarWithoutProfile(
+                            constraints, _back, "Notification"),
+                        textWidgetCentered("wähle deine Zeit aus"),
+                        textWidgetCentered(
+                            "aktuelle Zeit: ${time!.hour.toString()}:${time!.minute.toString()}"),
+                        Widgets.getButtonStyleOrange(
+                            _setTime, constraints, "Zeit setzen"),
+                        SizedBox(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        })));
+  }
+
+  TimeOfDay? stringToTime(String timeString) {
+    var timeInString = timeString.split(":");
+    print("timeSting" + timeString);
+    print("timeInSting" + timeInString.toString());
+    int minute = int.parse(timeInString[1].toString());
+    int hour = int.parse(timeInString[0].toString());
+    print("Setting new TimeOfDay with minute: " +
+        minute.toString() +
+        " hour:" +
+        hour.toString());
+    return TimeOfDay(hour: hour, minute: minute);
+  }
+
+  Future<void> setTimeFromStorage() async {
+    const storage = FlutterSecureStorage();
+    String timeString;
+    await storage.read(key: "notificationtime").then((value) => {
+          if (value != null)
+            {timeString = value, time = stringToTime(timeString), _renderNew()}
+        });
+  }
+
+  void _renderNew() {
+    setState(() {});
+  }
+
+  Future<void> _setTime() async {
+    TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: time!,
+    );
+    if (newTime != null) {
+      setState(() => {time = newTime});
+      const storage = FlutterSecureStorage();
+      await storage.write(
+          key: "notificationtime",
+          value: "${time!.hour.toString()}:${time!.minute.toString()}");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setTimeFromStorage();
+  }
+
+  void _back() {
+    Navigator.pop(context);
+  }
+
+  textWidgetCentered(String text) {
+    return Center(
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/profile_overview.dart
+++ lib/screens/profile_overview.dart
@@ -9,6 +9,7 @@ import '../api/exception/user_feedback_exception.dart';
 import '../route/route_generator.dart';
 import '../structs/profile.dart';
 import '../structs/team.dart';
+import '../widgets/settings.dart';
 
 class ProfileOverview extends StatefulWidget {
   const ProfileOverview({Key? key}) : super(key: key);
@@ -20,6 +21,7 @@ bool isLoading = true;
 
 List<Team> teams = [];
 Profile _profile = Profile.empty();
+bool isSettingsOpen = false;
 
 class _ProfileOverviewState extends State<ProfileOverview> {
   void apiCalls() async {
@@ -47,72 +49,153 @@ class _ProfileOverviewState extends State<ProfileOverview> {
               width: 50,
               height: 50,
             ))
-        : Scaffold(body: SafeArea(child: LayoutBuilder(builder: (builder, constraints) {
+        : Scaffold(body:
+            SafeArea(child: LayoutBuilder(builder: (builder, constraints) {
             _getTeams(constraints);
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            return Stack(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      alignment: Alignment.topLeft,
-                      margin: EdgeInsets.only(right: 10, top: 10),
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      //color: Settings.blueAccent,
-                      height: 60,
-                      width: 120,
-                      child: IconButton(
-                          onPressed: _back,
-                          icon: const Icon(
-                            Icons.arrow_back,
-                            color: Colors.black,
-                            size: 40,
-                          )),
-                    ),
-                    Center(
-                      child: Widgets.getNavHeaderText("Dein Profil", constraints),
-                    ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Container(
-                          margin: EdgeInsets.only(right: 10, top: 10),
-                          decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.blueAccent),
+                          alignment: Alignment.topLeft,
+                          margin: const EdgeInsets.only(right: 10, top: 10),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
                           //color: Settings.blueAccent,
                           height: 60,
                           width: 60,
-                          child: IconButton(onPressed: _openSettingsMenu, icon: Icon(Icons.settings, color: Colors.white, size: 40)),
+                          child: IconButton(
+                              onPressed: _back,
+                              icon: const Icon(
+                                Icons.arrow_back,
+                                color: Colors.black,
+                                size: 40,
+                              )),
                         ),
-                        Container(
-                          margin: EdgeInsets.only(right: 10, top: 10),
-                          decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.redAccent),
-                          //color: Settings.blueAccent,
-                          height: 60,
-                          width: 60,
-                          child: IconButton(onPressed: _logout, icon: Icon(Icons.logout, color: Colors.white, size: 40)),
+                        Center(
+                          child: Widgets.getNavHeaderText(
+                              "Dein Profil", constraints),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 10, top: 10),
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.blueAccent),
+                              //color: Settings.blueAccent,
+                              height: 60,
+                              width: 60,
+                              child: IconButton(
+                                  onPressed: _openSettingsMenu,
+                                  icon: Icon(Icons.settings,
+                                      color: Colors.white, size: 40)),
+                            ),
+                          ],
                         )
                       ],
-                    )
+                    ),
+                    Widgets.getProfilePictureInitials(
+                        _profile.getFullName(), true, constraints),
+                    Widgets.getTextFieldH2(_profile.getFullName(), constraints),
+                    Widgets.getTextFieldH3(_profile.email, constraints),
+                    displayTags(constraints),
+                    Container(
+                        margin:
+                            EdgeInsets.only(left: constraints.maxWidth * 0.05),
+                        child: Align(
+                            child: Widgets.getTextFieldH3(
+                                "Deine Teams:", constraints),
+                            alignment: Alignment.centerLeft)),
+                    Expanded(
+                        child: SingleChildScrollView(
+                      child: _getTeams(constraints),
+                    ))
                   ],
                 ),
-                Widgets.getProfilePictureInitials(_profile.getFullName(), true, constraints),
-                Widgets.getTextFieldH2(_profile.getFullName(), constraints),
-                Widgets.getTextFieldH3(_profile.email, constraints),
-                displayTags(constraints),
-                Container(
-                    margin: EdgeInsets.only(left: constraints.maxWidth * 0.05),
-                    child: Align(child: Widgets.getTextFieldH3("Deine Teams:", constraints), alignment: Alignment.centerLeft)),
-                Expanded(
-                    child: SingleChildScrollView(
-                  child: _getTeams(constraints),
-                ))
+                //Settings
+                getSettings(constraints),
               ],
             );
           })));
+  }
+
+  Widget getSettings(BoxConstraints constraints) {
+    if (!isSettingsOpen) {
+      return Column(
+        children: [
+          SizedBox(
+            height: 1,
+          ),
+        ],
+      );
+    } else {
+      return Column(
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Material(
+            color: Colors.black.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(50),
+            child: Container(
+              padding: EdgeInsets.only(top: 10),
+              //color: Colors.black.withOpacity(0.5),
+              child: Column(
+                children: [
+                  getMiniButton(
+                      constraints, "Profil bearbeiten", _openProfileEdit),
+                  getMiniButton(constraints, "Benachrichtigung bearbeiten",
+                      _openNotificationSettings),
+                  getMiniButton(constraints, "Abmelden", _logout),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+  }
+
+  Widget getMiniButton(
+      BoxConstraints constraints, String text, VoidCallback callback) {
+    return Container(
+        margin: EdgeInsets.only(bottom: constraints.maxWidth * 0.02),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              // margin: EdgeInsets.only(left: constraints.maxWidth * 0.05),
+              width: constraints.maxWidth * 0.90,
+              child: Material(
+                color: Settings.blueAccent,
+                borderRadius: BorderRadius.circular(50),
+                child: InkWell(
+                  onTap: callback,
+                  borderRadius: BorderRadius.circular(50),
+                  child: Container(
+                    height: constraints.maxWidth * 0.15,
+                    alignment: Alignment.center,
+                    child: Text(
+                      text,
+                      style: const TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Settings.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 
   //Teams
@@ -138,7 +221,8 @@ class _ProfileOverviewState extends State<ProfileOverview> {
       child: ListView.separated(
           scrollDirection: Axis.horizontal,
           shrinkWrap: true,
-          separatorBuilder: (BuildContext context, int index) => const Divider(),
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
           itemCount: _profile.tags.length,
           itemBuilder: (context, int index) {
             print(_profile.tags[index]);
@@ -153,8 +237,17 @@ class _ProfileOverviewState extends State<ProfileOverview> {
   }
 
   void _openSettingsMenu() {
-    print("Opensettings todo");
-    //TODO: Create a openSetting Menu
+    isSettingsOpen = !isSettingsOpen;
+    setState(() {});
+  }
+
+  void _openNotificationSettings() {
+    Navigator.pushReplacementNamed(
+        context, RouteGenerator.personalNotificationSetting);
+  }
+
+  void _openProfileEdit() {
+    setState(() {});
   }
 
   Future<void> _logout() async {
@@ -174,12 +267,14 @@ class _ProfileOverviewState extends State<ProfileOverview> {
   }
 
   void _goToTeam(Team team) {
-    Navigator.popAndPushNamed(context, RouteGenerator.teamDetails, arguments: {"team": team, "leader": team.leader});
+    Navigator.popAndPushNamed(context, RouteGenerator.teamDetails,
+        arguments: {"team": team, "leader": team.leader});
   }
 
   @override
   void initState() {
     apiCalls();
     super.initState();
+    isSettingsOpen = false;
   }
 }
